a67c9cd647b9199e3ed184f0ffaa7a8dae4a1023
william <18371143179@163.com>
2020-10-08 15:28:17 +0800

    第10章，call和ret，实验任务

--- chapter-10/t4.asm
+++ chapter-10/t4.asm
@@ -10,7 +10,7 @@ data ends
 
 ; @stack-segment
 stack segment
-    db 128 dup (0)
+    db 1024 dup (0)
 stack ends
 
 ; @main-segment
@@ -19,7 +19,7 @@ code segment
     start:
         mov ax, stack
         mov ss, ax
-        mov sp, 128                            ; 设置栈指针
+        mov sp, 1024                   ; 设置栈指针
 
         ; ; @display_clean
         ; mov ax, display_clean
@@ -41,19 +41,33 @@ code segment
         ; call dword ptr ds:[17]                 ; 调用子程序display_str
 
         ; @display_str
-        ; mov cl, 01110001b                      ; 白底蓝色
-        ; mov dl, 32
-        ; mov dh, 13                             ; 显示的行与列，从13行32列开始显示
-        ; mov ax, data
-        ; mov ds, ax                             
-        ; mov si, 0                              ; 设置数据段指针
-        ; call display_str
+        mov cl, 01110001b              ; 白底蓝色
+        mov dl, 32
+        mov dh, 13                     ; 显示的行与列，从13行32列开始显示
+        mov ax, data
+        mov ds, ax                             
+        mov si, 0                      ; 设置数据段指针
+        call display_str
 
         ; @divdw
         mov ax, 1000h
         mov dx, 1
         mov cx, 1
         call divdw
+
+        ; @dtoc
+        mov ax, data
+        mov ds, ax
+        mov si, 17
+        mov ax, 9e55h
+        mov dx, 98h
+        call dtoc
+
+        ; @display_str
+        mov dh, 8
+        mov dl, 3
+        mov cl, 2
+        call display_str
 
         mov ax, 4c00h
         int 21h
@@ -111,9 +125,10 @@ code segment
     ; int()：描述性运算符，取商，比如 int(38/10)=3
     ; rem()：描述性运算符，取余数，比如 rem(38/10)=8
     ; 公式：X/N = int(H/N) * 65536 + [rem(H/N) * 65536 + L] / N
+    ; 思想：将一个多位数的除法运算分解为多个少位数的除法运算
     ; 功能：进行不会产生溢出的除法运算，被除数为dword型，除数为word型，结果为dword型
     ; 参数：(ax)=dword型数据的低16位，(dx)=dword型数据的高16位，(cx)=除数
-    ; 返回：(dx)=结果的高16位，(ax)=结果的低16位，(cx)=余数
+    ; 返回：(ax)=结果的低16位，(dx)=结果的高16位，(cx)=余数
     divdw:
         push bx
         push si                        ; 保存父程序环境
@@ -135,6 +150,54 @@ code segment
         pop si
         pop bx
         ret                            ; 返回并恢复父程序环境
+
+    ; @subroutine
+    ; 名称：dtoc
+    ; 功能：将dword型数据转变为表示十进制数的字符串，字符串以0为结尾符
+    ; 参数：(ax)=dword型数据的低16位，(dx)=dword型数据的高16位，ds:si指向字符串的首地址
+    ; 返回：无
+    dtoc:
+        push ax
+        push bx
+        push cx
+        push dx
+        push si                        ; 保存父程序环境
+        mov bx, 0
+
+    dtoc_s:
+        mov cx, 10
+        call divdw
+        inc bx                         ; 记录求值的次数
+        push cx                        ; 保存余数
+
+    dtoc_censor_high_bit:
+        mov cx, dx
+        jcxz dtoc_censor_low_bit       ; 检查商的高位是否为0，为零则跳转检查商的低位
+        jmp dtoc_s                     ; 继续求值
+        
+    dtoc_censor_low_bit:
+        mov cx, ax
+        jcxz dtoc_ok                   ; 检查商的低位是否为0，如果高位和低位都为零，则表示以除尽
+        jmp dtoc_s                     ; 继续求值
+        
+    dtoc_ok:
+        mov cx, bx                     ; 字符串长度
+    dtoc_s1:
+        pop ax
+        add ax, 30h                    ; 求值的ASCII码
+        mov ds:[si], al                ; 保存值的ASCII码到内存中
+        inc si                         ; 指针加1
+        loop dtoc_s1                   ; 继续转换十进制值为ASCII码
+
+        mov ax, 0
+        mov ds:[si], al                ;  字符串以0结尾，添加结束标识
+
+        pop si
+        pop dx
+        pop cx
+        pop bx
+        pop ax
+        ret                            ; 恢复父程序环境并返回
 
 code ends
 end start
